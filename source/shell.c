d64249b47243212062a250f7929db5c66c08c887
Allina Veb <1910@mail.ru>
2021-10-30 23:50:05 +0600

    fixed fun cmpstring(check_end), launch, memclear; optimized get_word, get_list, main; have problems with wrong arguments execvp and spaces in end of string

--- source/shell.c
+++ source/shell.c
@@ -1,26 +1,42 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+/*void func_read_txt(char c){
+	char *txt = NULL;
+	txt = get_word;
+	int fd = open(&txt, flags);
+	if(c == '>'){
+		dup2();
+	}else{
+		dup2();
+	}
+}
+*/
 char *get_word (char *end){
         int counter = 0, bytes;
         char c, *word = NULL;
 	c = getchar();
+	while(c == ' ' || c == '\t'){
+		c = getchar();
+	}
+//	if(c == '<' || c == '>'){
+//		func_read_txt(c);
+//	}
         while(1){
+		bytes = (counter + 1) * sizeof(char);
+                word = realloc(word, bytes);
                 if(c == ' ' || c == '\t' || c == '\n'){
-			bytes = (counter + 1) * sizeof(char);
-                        word = realloc(word, bytes);
-                        *end = c;
-                        word[counter] = '\0';
-                        return word;
+			*end = c;
+			word[counter] = '\0';
+			return word;
                 }
                 else {
-			bytes = (counter + 1) * sizeof(char);
-			word = realloc(word, bytes);
                         word[counter] = c;
                         counter++;
                         c = getchar();
@@ -28,12 +44,10 @@ char *get_word (char *end){
         }
 }
 
-int cmpstring(const char *string){
-        char exit[] = "exit";
-        char quit[] = "quit";
+int check_end(char *string){
         int ans1, ans2;
-        ans1 = strcmp(string, exit);
-        ans2 = strcmp(string, quit);
+        ans1 = strcmp(string, "exit");
+        ans2 = strcmp(string, "quit");
         if(ans1 == 0 || ans2 == 0){
                 return 1;
         }
@@ -42,36 +56,32 @@ int cmpstring(const char *string){
         }
 }
 
-void launch(char *string){
+int launch(char **command){
 	if(fork() == 0){
-		execlp(string, string, NULL);
+		if(execvp(*command, command) == -1){
+			perror("incorrect command");
+			return 1;
+		}
 	}
 	wait(NULL);
+	return 0;
 }
 
 char **get_list(){
 	char end = 0, **string = NULL;
 	int size = 0, bytes;
         while(end != '\n'){
-		bytes = (size + 1) * sizeof(char*);
+		bytes = (size + 2) * sizeof(char*);
 		string = realloc(string, bytes);
 		string[size] = get_word(&end);
 		size++;
         }
-	bytes = (size + 1) * sizeof(char*);
-	string = realloc(string, bytes);
-	string[size] = '\0';
-	int i = 0;
-	while(1){
-		if(cmpstring(string[i])){
-                        break;
-		}
-		launch(string[i]);
-		i++;
+	if(string[size - 1] != NULL){
+		string[size] = NULL;
+	}else{
+		free(string[size]);
 	}
-	putchar('\n');
 	return string;
-
 }
 
 void memclear(char **list) {
@@ -79,12 +89,19 @@ void memclear(char **list) {
 	for(i = 0; list[i] != NULL; i++) {
 		free(list[i]);
 	}
-	free(list[i]);
 	free(list);
 }
 
 int main(int argc, char **argv) {
-	char **list = get_list();
-	memclear(list);
+	 while(1){
+		char **list = get_list();
+                if(check_end(*list)){
+                        break;
+                }
+                launch(list);
+        	putchar('\n');
+		memclear(list);
+	 }
         return 0;
 }
+
