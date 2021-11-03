e234d883e6284d80bd1b8d378bf91d48cb1f19cd
Allina Veb <1910@mail.ru>
2021-11-03 09:37:54 +0600

    space in end

--- source/shell.c
+++ source/shell.c
@@ -8,22 +8,23 @@
 #include <string.h>
 #define DEF_MODE S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH 
 
-char *get_word (char *end){
-        int counter = 0, bytes, sing = 0;
+char *get_word (char *end, int **sing){
+        int counter = 0, bytes;
         char c, *word = NULL;
 	c = getchar();
 	while(c == ' ' || c == '\t'){
 		c = getchar();
 	}
-/*	if(c == '<' || c == '>'){
+	if(c == '<' || c == '>'){
 		if(c == '>'){
-			sing++;
+			**sing = 1;
 		}else{
-			sing--;
+			**sing = -1;
 		}
-		c = gethar();
+		c = getchar();
+		return word;
 	}
-*/
+
         while(1){
 		bytes = (counter + 1) * sizeof(char);
                 word = realloc(word, bytes);
@@ -63,19 +64,24 @@ void launch(char **command){
 	
 }
 
-char **get_list(){
+char **get_list(int *sing){
 	char end = 0, **string = NULL;
 	int size = 0, bytes;
         while(end != '\n'){
 		bytes = (size + 2) * sizeof(char*);
 		string = realloc(string, bytes);
-		string[size] = get_word(&end);
+		string[size] = get_word(&end, &sing);
 		size++;
         }
-	if(string[size - 1] != NULL){
-		string[size] = NULL;
-	}else{
+	printf("%s pusto?\n", string[size - 1]);
+	printf("%c first symbol\n", string[size - 1][0]);
+	if(string[size - 1][0] == '\0'){
+		string[size - 1][0] = '\n';
+		printf("last word null\n");
 		free(string[size]);
+	}else{
+		string[size] = NULL;
+		printf("last word ne null\n");
 	}
 	return string;
 }
@@ -88,13 +94,30 @@ void memclear(char **list) {
 	free(list);
 }
 
+ /*void forwarding(int sing){
+	char **file = get_list(&sing);
+        int fd = open(**file, O_RDONLY | O_WRONLY, DEF_MODE);
+        if(sing > 0){
+		dup2(1, in file);
+        }else{
+		dup2(0, out file);
+                        }
+
+}
+*/
 int main(int argc, char **argv) {
 	 while(1){
-		char **list = get_list();
+		int sing = 0;
+		char **list = get_list(&sing);
                 if(check_end(*list)){
                         break;
                 }
-		launch(list);
+//		if(sing){
+//			forwarding(sing);
+//		}
+		if(list != NULL){
+			launch(list);
+		}
         	putchar('\n');
 		memclear(list);
 	 }
