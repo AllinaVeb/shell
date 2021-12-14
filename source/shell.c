6c8c37854cce01dea217f373c50419b08baf0d80
Allina Veb <1910@mail.ru>
2021-12-14 21:53:43 +0600

    5 part : pipe n

--- source/shell.c
+++ source/shell.c
@@ -46,11 +46,14 @@ void launch(char **command){
 			exit(1);
 		}
 	}
-	wait(NULL);
+	int wstatus;
+	wait(&wstatus);
+//	printf("%d\n", WEXITSTATUS(wstatus));
+//	wait(NULL);
 	
 }
 
-char **get_list(int *sign, int *sign_pipe){
+char **get_list(int *sign, int *sign_pipe, int *sign_and){
 	char end = 0, c, **string = NULL;
 	int size = 0, bytes;
         while(end != '\n'){
@@ -76,6 +79,11 @@ char **get_list(int *sign, int *sign_pipe){
 		}else{
 			break;
 		}
+		if(strcmp(string[size], "&&") == 0){
+			printf("read &&");
+			*sign_and = 1;
+			break;
+		}
         }
 	string[size] = NULL;
 	return string;
@@ -90,8 +98,8 @@ void memclear(char **list) {
 }
 
 int forwarding(int sign, char **list){
-	int sign2 = 0, sign_pipe = 0; //need for correct get_list for file
-	char **file = get_list(&sign2, &sign_pipe);
+	int sign2 = 0, sign_pipe = 0, sign_and = 0; //need for correct get_list for file
+	char **file = get_list(&sign2, &sign_pipe, &sign_and);
       	if(sign > 0){
 		int fd = open(file[0], O_WRONLY | O_CREAT, DEF_MODE);
 		if(fd < 0){
@@ -103,7 +111,8 @@ int forwarding(int sign, char **list){
 		close(fd);
 		dup2(savefd, 1);
 		close(savefd);
-	}else{ if(sign < 0){
+	}else{ 
+		if(sign < 0){
 			int fd = open(file[0], O_RDONLY, S_IROTH);
 			if(fd < 0){
                	 		return 0;
@@ -121,9 +130,61 @@ int forwarding(int sign, char **list){
 }
 
 void creatpipe(char **cmd1, int sign1) {
-	int fd[2], sign2 = 0, sign_pipe1 = 0; //sign_pipe1 = 0 for correct read cmd2
+//	char ***cmd = NULL;
+	int size = 0, bytes = 0, i = 0, fd[i][2];
+	char **cmd = NULL;
+	pipe(fd[i]);
+	if(fork() == 0){
+		dup2(fd[i][1],1);
+		close(fd[i][1]);
+		close(fd[i][0]);
+		launch(cmd1);
+	}
+	close(fd[i][1]);
+	wait(NULL);
+	memclear(cmd1);
+	for(i = 1; 1; i++){
+//		bytes = (size + 2) * sizeof(char**);
+//		printf(" bytes  =  %d\n", bytes);
+//		cmd = realloc(cmd, bytes);
+		int sign = 0, sign_pipe = 0, sign_and = 0;//for correct call get_list
+		cmd = get_list(&sign, &sign_pipe, &sign_and);
+//		size++;
+		char **pipe_sign = get_list(&sign, &sign_pipe, &sign_and);
+		if(!sign_pipe){
+			memclear(pipe_sign);
+			break;
+		}
+		memclear(pipe_sign);
+		pipe(fd[i]);
+		if(fork() == 0){
+			dup2(fd[i - 1][0], 0);
+			close(fd[i - 1][0]);
+			dup2(fd[i][1], 1);
+			close(fd[i][1]);
+			close(fd[i][0]);
+			launch(cmd);
+		}
+		memclear(cmd);
+		close(fd[i - 1][0]);
+		close(fd[i][1]);
+		wait(NULL);
+	}
+	dup2(fd[i][0], 0);
+	close(fd[i][0]);
+	launch(cmd);
+//	cmd[i + 1] = NULL;
+//	for(i = 1; cmd[i] != NULL; i++){
+//		memclear(cmd[i]);
+//	}
+	memclear(cmd);
+}
+
+
+/*
+	int fd[2], sign2 = 0, sign_pipe1 = 0, sign_and = 0; //sign_pipe1 = 0 for correct read cmd2
 	pipe(fd);
-	char **cmd2 = get_list(&sign2, &sign_pipe1);
+	char **cmd2 = get_list(&sign2, &sign_pipe1, &sign_and);
 	if(fork() == 0){
 		dup2(fd[1], 1);
 		close(fd[0]);
@@ -155,21 +216,9 @@ void creatpipe(char **cmd1, int sign1) {
 	wait(NULL);
 	wait(NULL);
 	memclear(cmd2);
-//	testpipe2 = get_list(&sign, &sign_pipe2);
-//	if(sign_pipe2)
-/*	if(fork() == 0){
-		int savefd = dup(1);
-		dup2(fd[1], 1);
-		close(fd[0]);
-		launch(cmd1);
-		dup2(savefd, 1);
-	}
-	dup2(fd[0], 0);
-	close(fd[1]);
-	wait(NULL);
-	return 0;
-	*/
 }
+*/
+
 void change_dir(char **cmd, char *home){
 	char a[100];
 	if(cmd[1] == NULL || strcmp(cmd[1], "~") == 0){
@@ -179,11 +228,20 @@ void change_dir(char **cmd, char *home){
 	}
 	printf("%s\n", getcwd(a,100));
 }
+
+void handler(int signo){
+
+	puts("received SIGINT");
+}
+
 int main(int argc, char **argv){
 	char *home = getenv("HOME");
+	signal(SIGINT, handler);
+//	int sign_and = 1;//for enter in
 	while(1){
-		int sign = 0, sign_pipe = 0;
-		char **list = get_list(&sign, &sign_pipe);
+		int sign = 0, sign_pipe = 0, sign_and = 0;
+		char **list = get_list(&sign, &sign_pipe, &sign_and);
+		printf(" sign && %d\n", sign_and);
                 if(check_end(*list)){
 			memclear(list);
 			break;
@@ -192,8 +250,8 @@ int main(int argc, char **argv){
 			change_dir(list, home);
 		}else{
 			if(sign_pipe){
-                                creatpipe(list, sign);
-                        }else{
+                       	        creatpipe(list, sign);
+			}else{
 				if(sign){
 				forwarding(sign, list);	
 				}else{
