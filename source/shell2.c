e32209ca0cac99eb5bc1818fde8ebece885f6472
Allina Veb <1910@mail.ru>
2021-12-16 20:53:56 +0600

    shell2 : read, exit, clear

---
+++ source/shell2.c
@@ -0,0 +1,118 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char *get_word(char *end, int *sign_pipe){
+	char *word = NULL;
+	int size = 0;
+	char c = getchar();
+	while(c == ' ' || c == '\t'){
+		c = getchar();
+	}
+	while(1){
+		if(c == ' ' || c == '\t' || c == '\n'){
+			*end = c;
+			if(size == 0){
+				return NULL;
+			}
+			word[size] = '\0';
+			return word;
+		}
+		if(c == '|'){
+		//	c = getchar();
+		//	if(c == ' '){
+				*sign_pipe = 1;
+				return NULL;
+		//	}
+		//	else{
+//				sign_or = 1;
+//			}
+		}
+		word = realloc(word, (size + 2) * sizeof(char));
+		word[size] = c;
+		size++;
+		c = getchar();
+	}
+}
+
+char **get_list(char *end, int *sign_pipe, int *num_cmd){
+	char **list = NULL;
+	int size = 0;
+	while(1){
+		list = realloc(list, (size + 2) * sizeof(char*));
+		list[size] = get_word(end, sign_pipe);
+		if(list[0] == NULL){
+			return NULL;
+		}
+		size++;
+		if(*end == '\n'){
+			list[size] = NULL;
+			*num_cmd = size;
+			return list;
+		}
+	}
+}
+
+char ***get_cmd(int *num_cmd){
+	char end = 0, ***cmd = NULL;
+	int size = 0, sign_pipe = 0;
+	while(1){
+		cmd = realloc(cmd, (size + 2) * sizeof(char**));
+		cmd[size] = get_list(&end, &sign_pipe, num_cmd);
+		if(cmd[0] == NULL){
+			return NULL;
+		}
+		if(sign_pipe){
+			printf("we are in get_cmd, sign_pipe = 1\n");
+		}
+		size++;
+		if(end == '\n'){
+			cmd[size] = NULL;
+			return cmd;
+		}
+	}
+}
+
+int check_end(char ***cmd){
+	if(strcmp(*cmd[0], "exit") == 0 || strcmp(*cmd[0], "quit") == 0){
+		return 1;
+	}
+	return 0;
+}
+
+void memclear(char ***cmd, int size){
+	for(int i = 0; cmd[i] != NULL; i++){
+		for(int j = 0; j < size; j++){
+		free(cmd[i][j]);
+		}
+		free(cmd[i]);
+	}
+	free(cmd);
+}
+
+int main(int argc, char **argv){
+	while(1){
+		int size = 0;
+		char ***cmd = get_cmd(&size);
+		while(cmd == NULL){
+			cmd = get_cmd(&size);
+		}
+		if(check_end(cmd)){
+			memclear(cmd, size);
+			break;
+		}
+		for(int i = 0; cmd[i]; i++){
+			for(int j = 0; j < size; j++){
+				printf("cmd[i = %d][j = %d] = %s\n", i, j, cmd[i][j]);
+			}
+		}	
+		putchar('\n');
+		memclear(cmd, size);
+	}
+	return 0;
+}
