0e2a4b1f7f34b6b23c754da75314654a89be6f45
Allina Veb <1910@mail.ru>
2021-12-17 22:36:33 +0600

    shell2: have single exec, cd. have conv_and but with error addressSanitize

--- source/shell2.c
+++ source/shell2.c
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char *get_word(char *end, int *sign_pipe){
+char *get_word(char *end, int *sign_pipe, int *sign_and){
 	char *word = NULL;
 	int size = 0;
 	char c = getchar();
@@ -30,8 +30,20 @@ char *get_word(char *end, int *sign_pipe){
 				return NULL;
 		//	}
 		//	else{
-//				sign_or = 1;
+//				*sign_or = 1;
+//				return NULL;
 //			}
+		}
+		if(c == '&'){
+			c = getchar();
+			if(c == '&'){
+				*sign_and = 1;
+				return NULL;
+//			}
+//			else{
+//				*sign_phone = 1;
+//				return NULL;
+			}
 		}
 		word = realloc(word, (size + 2) * sizeof(char));
 		word[size] = c;
@@ -40,35 +52,35 @@ char *get_word(char *end, int *sign_pipe){
 	}
 }
 
-char **get_list(char *end, int *sign_pipe, int *num_cmd){
+char **get_list(char *end, int *sign_pipe, int *sign_and){
 	char **list = NULL;
 	int size = 0;
 	while(1){
-		list = realloc(list, (size + 2) * sizeof(char*));
-		list[size] = get_word(end, sign_pipe);
+		list = realloc(list, (size + 1) * sizeof(char*));
+		list[size] = get_word(end, sign_pipe, sign_and);
 		if(list[0] == NULL){
 			return NULL;
 		}
+		if(list[size] == NULL){
+			return list;
+		}
 		size++;
 		if(*end == '\n'){
+			list = realloc(list, (size + 1) * sizeof(char*));
 			list[size] = NULL;
-			*num_cmd = size;
 			return list;
 		}
 	}
 }
 
-char ***get_cmd(int *num_cmd){
+char ***get_cmd(int *sign_pipe, int *sign_and){
 	char end = 0, ***cmd = NULL;
-	int size = 0, sign_pipe = 0;
+	int size = 0;
 	while(1){
 		cmd = realloc(cmd, (size + 2) * sizeof(char**));
-		cmd[size] = get_list(&end, &sign_pipe, num_cmd);
+		cmd[size] = get_list(&end, sign_pipe, sign_and);
 		if(cmd[0] == NULL){
 			return NULL;
-		}
-		if(sign_pipe){
-			printf("we are in get_cmd, sign_pipe = 1\n");
 		}
 		size++;
 		if(end == '\n'){
@@ -78,9 +90,9 @@ char ***get_cmd(int *num_cmd){
 	}
 }
 
-void memclear(char ***cmd, int size){
+void memclear(char ***cmd){
 	for(int i = 0; cmd[i] != NULL; i++){
-		for(int j = 0; j < size; j++){
+		for(int j = 0; cmd[i][j]; j++){
 		free(cmd[i][j]);
 		}
 		free(cmd[i]);
@@ -98,29 +110,53 @@ void change_dir(char ***cmd, char *home){
 	printf("%s\n", getcwd(a,100));
 }
 
+void conv_and(char ***cmd, int size){
+	for(int i = 0; i <= size; i++){
+		if(fork() == 0){
+			if(execvp(*cmd[i], cmd[i]) == -1){
+				perror("incorrect command");
+				exit(1);
+			}
+		}
+		wait(NULL);
+	}
+}
 
 int main(int argc, char **argv){
 	char *home = getenv("HOME");
+	int size;
 	while(1){
-		int size = 0;
-		char ***cmd = get_cmd(&size);
+		int sign_pipe = 0, sign_and = 0;
+		char ***cmd = get_cmd(&sign_pipe, &sign_and);
 		while(cmd == NULL){
-			cmd = get_cmd(&size);
+			cmd = get_cmd(&sign_pipe, &sign_and);
 		}
 		if(strcmp(*cmd[0], "exit") == 0 || strcmp(*cmd[0], "quit") == 0){
-			memclear(cmd, size);
+			memclear(cmd);
 			break;
 		}
 		if(strcmp(*cmd[0], "cd") == 0){
 			change_dir(cmd, home);
 		}
-		for(int i = 0; cmd[i]; i++){
-			for(int j = 0; j < size; j++){
-				printf("cmd[i = %d][j = %d] = %s\n", i, j, cmd[i][j]);
+		for(size = 0; cmd[size]; size++){
+			for(int j = 0; cmd[size][j]; j++){
+				printf("cmd[i = %d][j = %d] = %s\n", size, j, cmd[size][j]);
 			}
-		}	
+		}
+		if(sign_and){
+			conv_and(cmd, size);
+		}
+		else{
+			if(fork() == 0){
+				if(execvp(**cmd, *cmd) == -1){
+                       			perror("incorrect command");
+                       			exit(1);
+               			}
+			}
+			wait(NULL);
+		}
 		putchar('\n');
-		memclear(cmd, size);
+		memclear(cmd);
 	}
 	return 0;
 }
